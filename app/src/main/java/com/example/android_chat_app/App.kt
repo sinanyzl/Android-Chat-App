363ee873fe560e308396e1730619ddf4786c508f
Sinan Yozgatlı <sinanyozgatli01@gmail.com>
2023-05-29 08:48:55 +0300

    Initial commit

---
+++ app/src/main/java/com/example/android_chat_app/App.kt
@@ -0,0 +1,11 @@
+package com.example.android_chat_app
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class App : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
---
+++ app/src/main/java/com/example/android_chat_app/data/Result.kt
@@ -0,0 +1,4 @@
+package com.example.android_chat_app.data
+
+class Result {
+}

77c853b609cae11bd0087e407cd48777cc658895
Sinan Yozgatlı <sinanyozgatli01@gmail.com>
2023-05-31 19:38:32 +0300

    main

--- app/src/main/java/com/example/android_chat_app/data/db/entity/Chat.kt
+++ app/src/main/java/com/example/android_chat_app/data/db/entity/Chat.kt
@@ -1,4 +1,12 @@
 package com.example.android_chat_app.data.db.entity
 
-class Chat {
-}
+import com.google.firebase.database.PropertyName
+
+data class Chat (
+    @get:PropertyName("lastMessages") @set:PropertyName("lastMessages") var lastName: Message = Message(),
+    @get:PropertyName("info") @set:PropertyName("info") var info: ChatInfo = ChatInfo()
+)
+
+data class ChatInfo(
+    @get:PropertyName("id") @set:PropertyName("id") var id: String = ""
+)

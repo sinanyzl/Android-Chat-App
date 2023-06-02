ada5f0e4e6ef98c3bc210e660514e2a72f783f43
Sinan Yozgatlı <sinanyozgatli01@gmail.com>
2023-06-02 19:08:22 +0300

    main

--- app/src/main/java/com/example/android_chat_app/data/db/remote/FirebaseDatabaseSource.kt
+++ app/src/main/java/com/example/android_chat_app/data/db/remote/FirebaseDatabaseSource.kt
@@ -1,5 +1,19 @@
 package com.example.android_chat_app.data.db.remote
 
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
+
 class FirebaseDatabaseSource {
+
+    companion object{
+        val dbInstance = FirebaseDatabase.getInstance()
+    }
+
+    private fun refToPath(path: String): DatabaseReference{
+        return dbInstance.reference.child(path)
+    }
+
+
+
 
 }

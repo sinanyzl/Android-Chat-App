831f3e2d3af960e973d72cfba15aee83cf98f5b0
Sinan Yozgatlı <sinanyozgatli01@gmail.com>
2023-06-02 19:25:34 +0300

    main

--- app/src/main/java/com/example/android_chat_app/data/db/remote/FirebaseDatabaseSource.kt
+++ app/src/main/java/com/example/android_chat_app/data/db/remote/FirebaseDatabaseSource.kt
@@ -1,7 +1,16 @@
 package com.example.android_chat_app.data.db.remote
 
+import android.database.CursorJoiner
+import com.google.android.gms.tasks.TaskCompletionSource
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.Transaction
+import com.google.firebase.database.ValueEventListener
+import java.lang.Exception
+import com.google.android.gms.tasks.Task
+import com.google.firebase.database.*
 
 class FirebaseDatabaseSource {
 
@@ -12,6 +21,67 @@ class FirebaseDatabaseSource {
     private fun refToPath(path: String): DatabaseReference{
         return dbInstance.reference.child(path)
     }
+
+
+    private fun attachValueListenerToTaskCompletion(src: TaskCompletionSource<DataSnapshot>): ValueEventListener{
+        return (object : ValueEventListener{
+            override fun onCancelled(error: DatabaseError) {
+                src.setException(Exception(error.message))
+            }
+
+            override fun onDataChange(snapshot: DataSnapshot) {
+                src.setResult(snapshot)
+            }
+        })
+    }
+
+
+    private fun <T> attachValueListenerToBlock(resultClassName: Class<T>, b: ((Result<T>) -> Unit)): ValueEventListener{
+        return (object : ValueEventListener{
+            override fun onCancelled(error: DatabaseError) {
+                b.invoke(Result.Error(error.message))
+            }
+
+            override fun onDataChange(snapshot: DataSnapshot) {
+                if (wrapSnapshotToClass(resultClassName, snapshot) == null){
+                    b.invoke(Result.Error(msg = snapshot.key))
+                }else{
+                    b.invoke(Result.success(wrapSnapshotToClassName, snapshot))
+                }
+            }
+        })
+    }
+
+    private fun <T> attachValueListenerToBlockWithList(resultClassName: Class<T>, b: ((Result<MutableList<T>>)-> Unit)): ValueEventListener{
+
+        return (object : ValueEventListener {
+            override fun onCancelled(error: DatabaseError) {
+                b.invoke(Result.Error(error.message))
+            }
+
+            override fun onDataChange(snapshot: DataSnapshot) {
+                b.invoke(Result.success(wrapSnapshotToArrayList(resultClassName, snapshot)))
+            }
+        })
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 

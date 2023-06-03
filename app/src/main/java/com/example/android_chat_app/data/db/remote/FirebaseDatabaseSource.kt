c1da0f3dc54cfb2272cf28a8a5a2708d2a57224b
Sinan Yozgatlı <sinanyozgatli01@gmail.com>
2023-06-03 10:56:10 +0300

    main

--- app/src/main/java/com/example/android_chat_app/data/db/remote/FirebaseDatabaseSource.kt
+++ app/src/main/java/com/example/android_chat_app/data/db/remote/FirebaseDatabaseSource.kt
@@ -1,6 +1,12 @@
 package com.example.android_chat_app.data.db.remote
 
 import android.database.CursorJoiner
+import android.os.Message
+import com.example.android_chat_app.data.db.entity.Chat
+import com.example.android_chat_app.data.db.entity.User
+import com.example.android_chat_app.data.db.entity.UserFriend
+import com.example.android_chat_app.data.db.entity.UserNotification
+import com.example.android_chat_app.data.db.entity.UserRequest
 import com.google.android.gms.tasks.TaskCompletionSource
 import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
@@ -46,7 +52,7 @@ class FirebaseDatabaseSource {
                 if (wrapSnapshotToClass(resultClassName, snapshot) == null){
                     b.invoke(Result.Error(msg = snapshot.key))
                 }else{
-                    b.invoke(Result.success(wrapSnapshotToClassName, snapshot))
+                    b.invoke(Result.Success(wrapSnapshotToClassName, snapshot))
                 }
             }
         })
@@ -65,8 +71,57 @@ class FirebaseDatabaseSource {
         })
     }
 
+    private fun <T> attachChildListenerToBlock(resultClassName: Class<T>, b: ((Result<MutableList<T>>) -> Unit)): ValueEventListener{
+        return (object : ChildEventListener {
+            override fun onChildAdded(snapshot: DataSnapshot, previousChildName: String?) {
+                b.invoke(Result.Success(wrapSnapshotToClass, snapshot))
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                b.invoke(Result.Error(error.message))
+            }
+
+            override fun onChildMoved(snapshot: DataSnapshot, previousChildName: String?) { }
+            override fun onChildChanged(snapshot: DataSnapshot, previousChildName: String?) { }
+            override fun onChildRemoved(snapshot: DataSnapshot) { }
+        })
+    }
 
 
+    fun updateUserProfileImageUrl(userID: String, url: String){
+        refToPath("users/$userID/info/profileImageUrl").setValue(url)
+    }
+
+    fun updateUserStatus(userID: String, url: String){
+        refToPath("users/$userID/info/status").setValue(url)
+    }
+
+    fun updateLastMessage(chatId: String, message: Message){
+        refToPath("chats/$chatId/lastMessage").setValue(message)
+    }
+
+
+    fun updateNewFriend(myUser: UserFriend, otherUser: UserFriend){
+        refToPath("users/${myUser.userID}/friends/${otherUser.userID}").setValue(otherUser)
+        refToPath("users/${otherUser.userID}/friends/${myUser.userID}").setValue(myUser)
+    }
+
+    fun updateNewSentRequest(userID: String, userRequest: UserRequest){
+        refToPath("users/${userID}/sentRequest/${userRequest}").setValue(userRequest)
+    }
+
+    fun updateNewNotification(otherUserID: String, userNotification: UserNotification){
+        refToPath("users/${otherUserID}/sentRequest/${userNotification.userID}").setValue(userNotification)
+    }
+
+
+    fun updateNewUser(user: String){
+        refToPath("users/${user.info.id}").setValue(user)
+    }
+
+    fun updateNewChat(chat: Chat){
+        refToPath("chats/${chat.info.id}").setValue(chat)
+    }
 
 
 

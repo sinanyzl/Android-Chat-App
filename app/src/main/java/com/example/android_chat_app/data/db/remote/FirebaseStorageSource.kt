1b63500807c9f0361776ef7346a01227bfd0204f
Sinan Yozgatlı <sinanyozgatli01@gmail.com>
2023-06-03 14:17:02 +0300

    main

--- app/src/main/java/com/example/android_chat_app/data/db/remote/FirebaseStorageSource.kt
+++ app/src/main/java/com/example/android_chat_app/data/db/remote/FirebaseStorageSource.kt
@@ -1,7 +1,20 @@
 package com.example.android_chat_app.data.db.remote
 
-class FirebaseStorageSource {
+import android.net.Uri
+import com.google.android.gms.tasks.Task
+import com.google.firebase.storage.FirebaseStorage
 
+class FirebaseStorageSource {
+    private val storageInstance = FirebaseStorage.getInstance()
+
+    fun  uploadUserImage(userID: String, bArr: ByteArray): Task<Uri>{
+        val path = "user_photos/$userID/profile_image"
+        val ref = storageInstance.reference.child(path)
+
+        return ref.putBytes(bArr).continueWithTask{
+            ref.downloadUrl
+        }
+    }
 
 
 }

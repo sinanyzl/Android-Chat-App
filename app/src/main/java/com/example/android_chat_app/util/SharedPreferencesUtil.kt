99f899939e6c97dbc57c09da4671a91f887e5544
Sinan Yozgatlı <sinanyozgatli01@gmail.com>
2023-06-05 16:42:15 +0300

    main

--- app/src/main/java/com/example/android_chat_app/util/SharedPreferencesUtil.kt
+++ app/src/main/java/com/example/android_chat_app/util/SharedPreferencesUtil.kt
@@ -1,6 +1,29 @@
 package com.example.android_chat_app.util
 
-class SharedPreferencesUtil {
+import android.content.Context
+import android.content.SharedPreferences
+
+object SharedPreferencesUtil {
+
+    private const val PACK_NAME = "com.example.android_chat_app"
+    private const val KEY_USER_ID = "user_info"
+
+    private fun getPrefs(context: Context): SharedPreferences{
+        return context.getSharedPreferences(PACK_NAME, Context.MODE_PRIVATE)
+    }
+
+    fun getUserID(context: Context):String?{
+        return getPrefs(context).getString(KEY_USER_ID, null)
+    }
+
+    fun saveUserID(context: Context, userID: String){
+        getPrefs(context).edit().putString(KEY_USER_ID, userID).apply()
+    }
+
+    fun removeUserID(context: Context){
+        getPrefs(context).edit().remove(KEY_USER_ID).apply()
+    }
+
 
 
 }

3db13f6921376f192df0c5ee2539e7354bbbbd1c
Sinan Yozgatlı <sinanyozgatli01@gmail.com>
2023-06-05 16:44:45 +0300

    main

--- app/src/main/java/com/example/android_chat_app/util/TextUtil.kt
+++ app/src/main/java/com/example/android_chat_app/util/TextUtil.kt
@@ -1,4 +1,12 @@
 package com.example.android_chat_app.util
 
-class TextUtil {
+fun isEmailValid(email: CharSequence): Boolean {
+    return android.util.Patterns.EMAIL_ADDRESS.matcher(email).matches()
+}
+
+fun isTextValid(minLength: Int, text: String): Boolean{
+    if (text.isNullOrBlank() || text.length < minLength){
+        return false
+    }
+    return true
 }

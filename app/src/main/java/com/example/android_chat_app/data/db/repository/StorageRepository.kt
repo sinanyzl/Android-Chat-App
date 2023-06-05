08535c94b5d8a3547dc02254519bc52a09af7d6c
Sinan Yozgatlı <sinanyozgatli01@gmail.com>
2023-06-05 14:10:13 +0300

    main

--- app/src/main/java/com/example/android_chat_app/data/db/repository/StorageRepository.kt
+++ app/src/main/java/com/example/android_chat_app/data/db/repository/StorageRepository.kt
@@ -1,4 +1,18 @@
 package com.example.android_chat_app.data.db.repository
 
+import android.net.Uri
+import com.example.android_chat_app.data.db.remote.FirebaseStorageSource
+
 class StorageRepository {
+    private val firebaseStorageService = FirebaseStorageSource()
+
+    fun updateUserProfileImage(userID: String, byteArray: ByteArray, b: ((Result<Uri>) -> Unit)){
+        b.invoke(Result.Loading)
+        firebaseStorageService.uploadUserImage(userID, byteArray).addOnSuccessListener {
+            b.invoke((Result.Success(it)))
+        }.addOnFailureListener {
+            b.invoke(Result.Error(it.message))
+        }
+    }
+
 }

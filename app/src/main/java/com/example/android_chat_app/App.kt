cc82438a3c36b91a054cc9669b4d189940be9c54
Sinan Yozgatlı <sinanyozgatli01@gmail.com>
2023-06-05 16:57:13 +0300

    main

--- app/src/main/java/com/example/android_chat_app/App.kt
+++ app/src/main/java/com/example/android_chat_app/App.kt
@@ -1,11 +1,23 @@
 package com.example.android_chat_app
 
-import androidx.appcompat.app.AppCompatActivity
-import android.os.Bundle
+import android.app.Application
+import com.example.android_chat_app.util.SharedPreferencesUtil
 
-class App : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+class App : Application() {
+    override fun onCreate() {
+        super.onCreate()
+        application = this
+    }
+
+    companion object{
+        lateinit var application: Application
+            private set
+
+        var myUserID: String = ""
+            get() {
+                field = SharedPreferencesUtil.getUserID(application.applicationContext).orEmpty()
+                return field
+            }
+            private set
     }
 }
--- app/src/main/java/com/example/android_chat_app/util/ViewExt.kt
+++ app/src/main/java/com/example/android_chat_app/util/ViewExt.kt
@@ -1,4 +1,17 @@
 package com.example.android_chat_app.util
 
-class ViewExt {
+import android.content.Context
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import com.example.android_chat_app.R
+import com.google.android.material.snackbar.Snackbar
+
+fun View.forceHideKeyboard(){
+    val inputManager: InputMethodManager =
+        this.context.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+    inputManager.hideSoftInputFromWindow(this.windowToken, InputMethodManager.HIDE_NOT_ALWAYS)
+}
+
+fun View.showSnackBar(text: String){
+    Snackbar.make(this.rootView.findViewById(R.id.container), text, Snackbar.LENGTH_SHORT).show()
 }

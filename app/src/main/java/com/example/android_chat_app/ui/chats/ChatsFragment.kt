207d5c5d99fa46e888d754d088f3e62c21f81a6f
Sinan Yozgatlı <sinanyozgatli01@gmail.com>
2023-06-10 09:14:14 +0300

    main

--- app/src/main/java/com/example/android_chat_app/ui/chats/ChatsFragment.kt
+++ app/src/main/java/com/example/android_chat_app/ui/chats/ChatsFragment.kt
@@ -1,4 +1,13 @@
 package com.example.android_chat_app.ui.chats
 
+import android.view.View
+import android.widget.TextView
+import androidx.databinding.BindingAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.example.android_chat_app.R
+import com.example.android_chat_app.data.model.ChatWithUserInfo
+import com.example.android_chat_app.data.db.entity.Message
+
+
 class ChatsFragment {
 }

695c15e0dd84edccc7fd669e31689b737ab7af4e
Sinan Yozgatlı <sinanyozgatli01@gmail.com>
2023-06-10 18:30:21 +0300

    main

--- app/src/main/java/com/example/android_chat_app/ui/chats/ChatsFragment.kt
+++ app/src/main/java/com/example/android_chat_app/ui/chats/ChatsFragment.kt
@@ -5,6 +5,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.core.os.bundleOf
+import androidx.databinding.ViewDataBinding
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
@@ -14,8 +15,55 @@ import com.example.android_chat_app.data.model.ChatWithUserInfo
 import com.example.android_chat_app.databinding.FragmentChatsBinding
 import com.example.android_chat_app.data.EventObserver
 import com.example.android_chat_app.ui.chat.ChatFragment
+import com.example.android_chat_app.ui.chat.ChatViewModelFactory
 import com.example.android_chat_app.util.convertTwoUserIDs
 
 
-class ChatsFragment {
+class ChatsFragment: Fragment() {
+
+    private val  viewModel: ChatsViewModel by viewModels { ChatViewModelFactory(App.myUserID) }
+    private lateinit var viewDataBinding: FragmentChatsBinding
+    private lateinit var listAdapter: ChatsListAdapter
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        viewDataBinding =    FragmentChatsBinding.inflate(inflater, container, false).apply { viewmodel = viewModel }
+        viewDataBinding.lifecycleOwner = this.viewLifecycleOwner
+        return viewDataBinding.root
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        setupListAdapter()
+        setupObservers()
+    }
+
+    private fun setupListAdapter() {
+        val viewModel = viewDataBinding.viewmodel
+        if (viewModel != null) {
+            listAdapter = ChatsListAdapter(viewModel)
+            viewDataBinding.chatsRecyclerView.adapter = listAdapter
+        } else {
+            throw Exception("The viewmodel is not initialized")
+        }
+    }
+
+    private fun setupObservers() {
+        viewModel.selectedChat.observe(viewLifecycleOwner,
+            EventObserver { navigateToChat(it) })
+    }
+
+    private fun navigateToChat(chatWithUserInfo: ChatWithUserInfo) {
+        val bundle = bundleOf(
+            ChatFragment.ARGS_KEY_USER_ID to App.myUserID,
+            ChatFragment.ARGS_KEY_OTHER_USER_ID to chatWithUserInfo.mUserInfo.id,
+            ChatFragment.ARGS_KEY_CHAT_ID to convertTwoUserIDs(App.myUserID, chatWithUserInfo.mUserInfo.id)
+        )
+        findNavController().navigate(R.id.action_navigation_chats_to_chatFragment, bundle)
+    }
+
+
 }

1a238c5f89d338953e392b2a5c3cc3a90da62d14
Sinan Yozgatlı <sinanyozgatli01@gmail.com>
2023-06-10 09:16:27 +0300

    main

--- app/src/main/java/com/example/android_chat_app/ui/chats/ChatsFragment.kt
+++ app/src/main/java/com/example/android_chat_app/ui/chats/ChatsFragment.kt
@@ -1,12 +1,20 @@
 package com.example.android_chat_app.ui.chats
 
+import android.os.Bundle
+import android.view.LayoutInflater
 import android.view.View
-import android.widget.TextView
-import androidx.databinding.BindingAdapter
-import androidx.recyclerview.widget.RecyclerView
+import android.view.ViewGroup
+import androidx.core.os.bundleOf
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
+import com.example.android_chat_app.App
 import com.example.android_chat_app.R
 import com.example.android_chat_app.data.model.ChatWithUserInfo
-import com.example.android_chat_app.data.db.entity.Message
+import com.example.android_chat_app.databinding.FragmentChatsBinding
+import com.example.android_chat_app.data.EventObserver
+import com.example.android_chat_app.ui.chat.ChatFragment
+import com.example.android_chat_app.util.convertTwoUserIDs
 
 
 class ChatsFragment {

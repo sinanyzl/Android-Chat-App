b0c9a7e3e9be1c7eb4a47500e5f0941d9df6d52c
Sinan Yozgatlı <sinanyozgatli01@gmail.com>
2023-06-12 13:31:28 +0300

    main

--- app/src/main/java/com/example/android_chat_app/ui/users/UsersBinding.kt
+++ app/src/main/java/com/example/android_chat_app/ui/users/UsersBinding.kt
@@ -1,4 +1,10 @@
 package com.example.android_chat_app.ui.users
 
-class UsersBinding {
+import androidx.databinding.BindingAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.example.android_chat_app.data.db.entity.User
+
+@BindingAdapter("bind_users_list")
+fun bindUsersList(listView: RecyclerView, items: List<User>?) {
+    items?.let { (listView.adapter as UsersListAdapter).submitList(items) }
 }

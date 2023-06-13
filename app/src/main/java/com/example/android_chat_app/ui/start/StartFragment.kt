b3927a5a04dc9a29926d643238b908c4c957b05d
Sinan Yozgatlı <sinanyozgatli01@gmail.com>
2023-06-13 14:46:49 +0300

    main

--- app/src/main/java/com/example/android_chat_app/ui/start/StartFragment.kt
+++ app/src/main/java/com/example/android_chat_app/ui/start/StartFragment.kt
@@ -1,7 +1,66 @@
 package com.example.android_chat_app.ui.start
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
+import com.example.android_chat_app.R
+import com.example.android_chat_app.databinding.FragmentStartBinding
+import com.example.android_chat_app.data.EventObserver
+import com.example.android_chat_app.util.SharedPreferencesUtil
+
+
+class StartFragment : Fragment(){
+
+    private val viewModel by viewModels<StartViewModel>()
+    private lateinit var viewDatabinding: FragmentStartBingind
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
+    ): View? {
+        viewDataBinding =
+            FragmentStartBinding.inflate(inflater, container, false).apply { viewmodel = viewModel }
+        viewDataBinding.lifecycleOwner = this.viewLifecycleOwner
+        setHasOptionsMenu(false)
+        return viewDataBinding.root
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        setupObservers()
+
+        if (userIsAlreadyLoggedIn()) {
+            navigateDirectlyToChats()
+        }
+    }
+
+
+    private fun userIsAlreadyLoggedIn(): Boolean {
+        return SharedPreferencesUtil.getUserID(requireContext()) != null
+    }
+
+    private fun setupObservers() {
+        viewModel.loginEvent.observe(viewLifecycleOwner, EventObserver { navigateToLogin() })
+        viewModel.createAccountEvent.observe(
+            viewLifecycleOwner, EventObserver { navigateToCreateAccount() })
+    }
+
+    private fun navigateDirectlyToChats() {
+        findNavController().navigate(R.id.action_startFragment_to_navigation_chats)
+    }
+
+    private fun navigateToLogin() {
+        findNavController().navigate(R.id.action_startFragment_to_loginFragment)
+    }
+
+    private fun navigateToCreateAccount() {
+        findNavController().navigate(R.id.action_startFragment_to_createAccountFragment)
+    }
 
 
 
-class StartFragment {
 }

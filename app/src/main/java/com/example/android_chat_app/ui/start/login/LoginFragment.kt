6de7edc2d91844adb3a39b8b1b576eec287b8c18
Sinan Yozgatlı <sinanyozgatli01@gmail.com>
2023-06-13 13:50:37 +0300

    main

--- app/src/main/java/com/example/android_chat_app/ui/start/login/LoginFragment.kt
+++ app/src/main/java/com/example/android_chat_app/ui/start/login/LoginFragment.kt
@@ -1,4 +1,73 @@
 package com.example.android_chat_app.ui.start.login
 
-class LoginFragment {
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.MenuItem
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
+import com.example.android_chat_app.R
+import com.example.android_chat_app.databinding.FragmentLoginBinding
+import com.example.android_chat_app.data.EventObserver
+import com.example.android_chat_app.util.showSnackBar
+import com.example.android_chat_app.ui.main.MainActivity
+import com.example.android_chat_app.util.SharedPreferencesUtil
+import com.example.android_chat_app.util.forceHideKeyboard
+
+class LoginFragment : Fragment() {
+
+    private val viewModel by viewModels<LoginViewModel>()
+    private lateinit var viewDatabinding: FragmentLoginBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        viewDatabinding = FragmentLoginBinding.inflate(inflater, container, false)
+            .apply{viewmodel = viewModel}
+        viewDatabinding.lifecycleOwner = this.viewLifecycleOwner
+        setHasOptionsMenu(true)
+        return viewDatabinding.root
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        setupObservers()
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item.itemId){
+            android.R.id.home -> {
+                findNavController().popBackStack()
+                return true
+            }
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    private fun setupObservers(){
+        viewModel.dataLoading.observe(viewLifecycleOwner,
+            EventObserver{(activity as MainActivity).showGlobalProgressBar(it)})
+
+        viewModel.snackBarText.observe(viewLifecycleOwner,
+        EventObserver{text ->
+            view?.showSnackBar(text)
+            view?.forceHideKeyboard()
+        })
+
+        viewModel.isLoggedInEvent.observe(viewLifecycleOwner, EventObserver{
+            SharedPreferencesUtil.saveUserID(requireContext(), it.uid)
+            navigateToChats()
+        })
+    }
+
+    private fun navigateToChats(){
+        findNavController().navigate(R.id.action_loginFragment_to_navigation_chats)
+    }
+
+
 }

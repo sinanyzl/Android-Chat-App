28b65f265ce8ec5c99e052b924377dde3b76449d
Sinan Yozgatlı <sinanyozgatli01@gmail.com>
2023-06-13 14:47:23 +0300

    main

--- app/src/main/java/com/example/android_chat_app/ui/start/StartViewModel.kt
+++ app/src/main/java/com/example/android_chat_app/ui/start/StartViewModel.kt
@@ -1,4 +1,23 @@
 package com.example.android_chat_app.ui.start
 
-class StartViewModel {
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.example.android_chat_app.data.Event
+
+class StartViewModel : ViewModel() {
+
+    private val _loginEvent = MutableLiveData<Event<Unit>>()
+    private val _createAccountEvent = MutableLiveData<Event<Unit>>()
+
+    val loginEvent: LiveData<Event<Unit>> = _loginEvent
+    val createAccountEvent: LiveData<Event<Unit>> = _createAccountEvent
+
+    fun goToLoginPressed() {
+        _loginEvent.value = Event(Unit)
+    }
+
+    fun goToCreateAccountPressed() {
+        _createAccountEvent.value = Event(Unit)
+    }
 }

084b40bd8c615c688eec494329e9ac924e28e60e
Sinan Yozgatlı <sinanyozgatli01@gmail.com>
2023-06-15 13:46:55 +0300

    main

--- app/src/main/java/com/example/android_chat_app/ui/notifications/NotificationsBinding.kt
+++ app/src/main/java/com/example/android_chat_app/ui/notifications/NotificationsBinding.kt
@@ -1,4 +1,10 @@
 package com.example.android_chat_app.ui.notifications
 
-class NotificationsBinding {
+import androidx.databinding.BindingAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.example.android_chat_app.data.db.entity.UserInfo
+
+@BindingAdapter("bind_notifications_list")
+fun bindNotificationsList(listView: RecyclerView, items: List<UserInfo>?) {
+    items?.let { (listView.adapter as NotificationsListAdapter).submitList(items) }
 }
